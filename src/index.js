11e9a5498c25b6c270c7dfcd136404deec05f1d8
Yana <YanaLSoltan@gmail.com>
2018-02-23 13:52:09 +0300

    task 5

--- src/index.js
+++ src/index.js
@@ -8,14 +8,39 @@ module.exports = function makeExchange(currency) {
     var quarters = 25;
     var halfDoll = 50;
 
+    var ost = 0;
+
     if (currency <= 0) {
         return {};
     }
     if(currency > 10000) {
         return  {error: "You are rich, my friend! We don't have so much coins for exchange"}
     }
+    if(currency > halfDoll){
+        ost = Math.floor(currency / halfDoll);
+        pocket.H = ost;
+        currency == currency - (ost*halfDoll);
+}
+    if (currency > quarters) {
+        ost = Math.floor(currency / quarters);
+        pocket.Q = ost;
+        currency == currency - (ost*quarters);
 
+    }
+    if (currency > dimes) {
+        ost = Math.floor(currency / dime);
+        pocket.D = ost;
+        currency == currency - (ost*dimes);
+    }
+    if (currency > nickels) {
+        ost = Math.floor(currency / nickels);
+        pocket.N = ost;
+        currency == currency - (ost*nickels);
+    }
+    if (currency > pennie) {
+        ost = Math.floor(currency / pennie);
+        pocket.P = ost;
+        currency == currency - (ost*pennie);
+    }
     return pocket;
-
-
 }
