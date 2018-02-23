f223cab0cbca2911d8f6150ebd2d1d4fd02e4768
Yana <YanaLSoltan@gmail.com>
2018-02-23 14:19:30 +0300

    task 5

--- src/index.js
+++ src/index.js
@@ -1,46 +1,46 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
 
-    let money = {};
-    let halfDollar = 50;
-    let quarter = 25;
-    let dime = 10;
-    let nickel = 5;
-    let pennie = 1;
-    let reminder;
+    var pocket = new Array();
+    var pennie = 1;
+    var nickels = 5;
+    var dimes = 10;
+    var quarters = 25;
+    var halfDoll = 50;
 
-    if (currency > 10000) {
-        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
-    }
+    var ost = 0;
+
     if (currency <= 0) {
         return {};
     }
-    if (Math.floor(currency / halfDollar) > 0) {
-        reminder = Math.floor(currency / halfDollar);
-        money.H = reminder;
-        currency -= reminder * halfDollar;
+    if(currency > 10000) {
+        return  {error: "You are rich, my friend! We don't have so much coins for exchange"}
     }
-    if (Math.floor(currency / quarter) > 0) {
-        reminder = Math.floor(currency / quarter);
-        money.Q = reminder;
-        currency -= reminder * quarter;
+    if(currency > halfDoll){
+        ost = Math.floor(currency / halfDoll);
+        pocket.H = ost;
+        currency = currency - (ost*halfDoll);
+}
+    if (currency > quarters) {
+        ost = Math.floor(currency / quarters);
+        pocket.Q = ost;
+        currency = currency - (ost*quarters);
+
     }
-    if (Math.floor(currency / dime) > 0) {
-        reminder = Math.floor(currency / dime);
-        money.D = reminder;
-        currency -= reminder * dime;
+    if (currency > dimes) {
+        ost = Math.floor(currency / dimes);
+        pocket.D = ost;
+        currency = currency - (ost*dimes);
     }
-    if (Math.floor(currency / nickel) > 0) {
-        reminder = Math.floor(currency / nickel);
-        money.N = reminder;
-        currency -= reminder * nickel;
+    if (currency > nickels) {
+        ost = Math.floor(currency / nickels);
+        pocket.N = ost;
+        currency = currency - (ost*nickels);
     }
-    if (Math.floor(currency / pennie) > 0) {
-        reminder = Math.floor(currency / pennie);
-        money.P = reminder;
-        currency -= reminder * pennie;
+    if (currency > pennie) {
+        ost = Math.floor(currency / pennie);
+        pocket.P = ost;
+        currency = currency - (ost*pennie);
     }
-    return money;
+    return pocket;
 }
