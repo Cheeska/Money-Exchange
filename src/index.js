b0763bfc30f13627762be8c7a7b85c8dda44b014
Yana <YanaLSoltan@gmail.com>
2018-02-23 12:52:37 +0300

    task 5

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ module.exports = function makeExchange(currency) {
     if (currency <= 0) {
         return {};
     }
+    if(currency > 10000) {
+        return  {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }
 
     return pocket;
 
