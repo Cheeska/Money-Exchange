4807617a1d3949d5922abd22efec3e09b2fac2fd
Yana <YanaLSoltan@gmail.com>
2018-02-23 14:05:29 +0300

    task 5

--- src/index.js
+++ src/index.js
@@ -20,30 +20,31 @@ function makeExchange(currency) {
     if(currency > halfDoll){
         ost = Math.floor(currency / halfDoll);
         pocket.H = ost;
-        currency == currency - (ost*halfDoll);
+        currency = currency - (ost*halfDoll);
 }
     if (currency > quarters) {
         ost = Math.floor(currency / quarters);
         pocket.Q = ost;
-        currency == currency - (ost*quarters);
+        currency = currency - (ost*quarters);
 
     }
     if (currency > dimes) {
         ost = Math.floor(currency / dimes);
         pocket.D = ost;
-        currency == currency - (ost*dimes);
+        currency = currency - (ost*dimes);
     }
     if (currency > nickels) {
         ost = Math.floor(currency / nickels);
         pocket.N = ost;
-        currency == currency - (ost*nickels);
+        currency = currency - (ost*nickels);
     }
     if (currency > pennie) {
         ost = Math.floor(currency / pennie);
         pocket.P = ost;
-        currency == currency - (ost*pennie);
+        currency = currency - (ost*pennie);
     }
     return pocket;
 }
+
 
 console.log(makeExchange(70));
