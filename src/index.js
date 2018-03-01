5884dcd565c0c656562e7756284694c06a12d7b1
Yana <YanaLSoltan@gmail.com>
2018-03-01 12:15:42 +0300

    result

--- src/index.js
+++ src/index.js
@@ -16,31 +16,31 @@ module.exports = function makeExchange(currency) {
     if(currency > 10000) {
         return  {error: "You are rich, my friend! We don't have so much coins for exchange"}
     }
-    if(currency > halfDoll){
+    if(currency >= halfDoll){
         ost = Math.floor(currency / halfDoll);
         pocket.H = ost;
         currency = currency - (ost*halfDoll);
 }
-    if (currency > quarters) {
+    if (currency >= quarters) {
         ost = Math.floor(currency / quarters);
         pocket.Q = ost;
         currency = currency - (ost*quarters);
 
     }
-    if (currency > dimes) {
+    if (currency >= dimes) {
         ost = Math.floor(currency / dimes);
         pocket.D = ost;
         currency = currency - (ost*dimes);
     }
-    if (currency > nickels) {
+    if (currency >= nickels) {
         ost = Math.floor(currency / nickels);
         pocket.N = ost;
         currency = currency - (ost*nickels);
     }
-    if (currency > pennie) {
+    if (currency >=  pennie) {
         ost = Math.floor(currency / pennie);
         pocket.P = ost;
-        currency = currency - (ost*pennie);
+        //currency = currency - (ost*pennie);
     }
     return pocket;
 }
